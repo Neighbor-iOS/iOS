61de58b1c9c5c377381bfd4624b05fa9fab03d3f
Hakeniz <redhakeniz@naver.com>
2015-01-27 17:11:36 +0900

    Fix Image Error

---
+++ CollectionTest/CollectionTest/DetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.h
+//  CollectionTest
+//
+//  Created by 강민우 on 2015. 1. 27..
+//  Copyright (c) 2015년 강민우. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIImageView *DetailImage;
+
+@end
